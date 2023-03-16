bad81c52420dd973edbe16f141ffa20da17318de
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-03-16 15:13:14 +0300

    fix: page1 bugs & fix: adaptive

--- src/pages/Discover/Hotbid/Hotbid.jsx
+++ src/pages/Discover/Hotbid/Hotbid.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import HotbidCards from './HotbidCards'
+import React from "react";
+import HotbidCards from "./HotbidCards";
 
 const Hotbid = () => {
   return (
-    <div className='hotbid'>
-        <div className="container">
-            <div className="hotbid__inner">
-                <div className="hotbid__inner">
-                    <div className="hotbid__title disflex">
-                        <h2 className='title'>Hot Bid</h2>
-                        <div className='disflex acenter tcenter'>
-                            <b className='prev'></b>
-                            <b className='next'></b>
-                        </div>
-                    </div>
-                    <HotbidCards/>
-                </div>
+    <div className="hotbid">
+      <div className="container">
+        <div className="hotbid__inner">
+          <div className="hotbid__inner">
+            <div className="hotbid__title disflex">
+              <h2 className="title">Hot Bid</h2>
+              <div className="disflex acenter tcenter">
+                <b className="prev"></b>
+                <b className="next"></b>
+              </div>
             </div>
+            <div className="forScroll">
+              <HotbidCards />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hotbid
+export default Hotbid;
